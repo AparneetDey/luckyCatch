5582305ce0afc2c6cf19559d27cccdf3874edcaf
Aparneet Dey <aparnitd@gmail.com>
2026-09-20 00:38:42 +0530

    Animate rod/bobber line anchor points
    Adds explicit `LinePoint` anchors to the bobber setup and animates anchor positions in both rod and bobber scenes so the fishing line follows motion more naturally. `fishing.gd` now types Bobber explicitly and updates line endpoints using each anchor’s global position converted into `FishingLine` local space, fixing coordinate mismatch issues. Also adjusts bobber visual placement (sprite offset/scene position) and updates bobber art.

--- scenes/fishingRod/fishing.gd
+++ scenes/fishingRod/fishing.gd
@@ -2,7 +2,7 @@ class_name Fishing
 extends Node2D
 
 @onready var bamboo_rod: BambooRod = %BambooRod
-@onready var bobber: Node2D = %Bobber
+@onready var bobber: Bobber = %Bobber
 @onready var fishing_line: Line2D = %FishingLine
 
 # Called when the node enters the scene tree for the first time.
@@ -14,5 +14,7 @@ func _process(_delta: float) -> void:
 	handleFishingLine()
 
 func handleFishingLine() -> void:
-	fishing_line.set_point_position(0, bamboo_rod.line_point.global_position)
-	fishing_line.set_point_position(1, bobber.position)
+	var rod_point := fishing_line.to_local(bamboo_rod.line_point.global_position)
+	var bobber_point := fishing_line.to_local(bobber.line_point.global_position)
+	fishing_line.set_point_position(0, rod_point)
+	fishing_line.set_point_position(1, bobber_point)
